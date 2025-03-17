64ca17082c75b78853913ac8e72d7050b02542b1
m4t124 <matias.morales.02@alu.ucm.cl>
2025-03-16 21:40:46 -0300

    Documentación App

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import GameDetalle from "./pages/GameDetalle";
 
+//Este código es el componente principal, el cual configura las rutas utilizando React Router
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/game/:id" element={<GameDetalle />} />
+        <Route path="/" element={<Home />} /> {/* Ruta principal que muestra la lista de videojuegos */}
+        <Route path="/game/:id" element={<GameDetalle />} /> {/* Ruta de los detalles principales de un videojuego */}
       </Routes>
     </Router>
   );
